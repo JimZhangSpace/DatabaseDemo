9b32bec9ad4a261dd853b844b9420167662d8cc6
Jim Zhang <jzhang@midtronics.com>
2018-01-23 14:24:47 +0800

    update deflow_demo name

--- dbflow_demo/src/main/java/com/jzhang/android/dbflow_demo/AppDatabase.java
+++ dbflow_demo/src/main/java/com/jzhang/android/dbflow_demo/AppDatabase.java
@@ -9,7 +9,7 @@ import com.raizlabs.android.dbflow.annotation.Database;
 @Database(name = AppDatabase.NAME, version = AppDatabase.VERSION)
 
 public class AppDatabase {
-    public static final String NAME = "AppDatabase";
+    public static final String NAME = "dbflow_demo";
 
     public static final int VERSION = 2;
 }
